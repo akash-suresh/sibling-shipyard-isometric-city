0d79b365ab7c6b14516a4c5b93591e13021823a6
akash-suresh <akashsuresh36@gmail.com>
2026-08-22 11:04:53 +0100

    Fix orthogonal waypoint routing for drone-1

--- app/src/world/layout/townLayout.ts
+++ app/src/world/layout/townLayout.ts
@@ -104,7 +104,7 @@ export const shipyardZeroLayout: TownLayout = {
     {
       id: "drone-1",
       actor: "person",
-      waypoints: [point(4, 4), point(24, 8), point(16, 20), point(4, 4)],
+      waypoints: [point(4, 4), point(24, 4), point(24, 20), point(4, 20), point(4, 4)],
       durationMs: 25000,
       reducedProgress: 0,
       offset: point(0, 0),
